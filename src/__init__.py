2502315fa2100a1efc2e97e2eb3aa650bb6c2a61
thisIsOwais <mohdowaisansari608@gmail.com>
2026-05-19 11:55:19 +0530

    Add lifespan event to initialize database on server start

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,25 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+from src.db.main import initdb
 
-version = 'v1'
+#the lifespan event
+@asynccontextmanager
+async def lifespan(app: FastAPI):    
+    print("Server is starting...")
+    await initdb()  # Initialize the database when the server starts
+    yield
+    print("server is stopping")
+
+
+
+version = "1.0.0"
 
 app = FastAPI(
     title="Bookly",
     description="A REST API for a book review web service",
     version= version,
-    # lifespan=life_span
+    lifespan=lifespan
 )
 
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=['books'])
