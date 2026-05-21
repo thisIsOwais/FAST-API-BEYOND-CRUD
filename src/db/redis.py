4da82f4bd2cbee5eb8ea6eea882dcf5f1f061855
thisIsOwais <mohdowaisansari608@gmail.com>
2026-05-21 13:20:00 +0530

    Refactor Redis integration: switch from aioredis to redis.asyncio for token blocklist management and improve code readability as aioredis has been deprecated

--- src/db/redis.py
+++ src/db/redis.py
@@ -1,18 +1,44 @@
-import aioredis
+# import aioredis
+# from src.config import Config
+
+# JTI_EXPIRY = 3600
+
+# token_blocklist = aioredis.StrictRedis(
+#     host=Config.REDIS_HOST, port=Config.REDIS_PORT, db=0
+# )
+
+
+# async def add_jti_to_blocklist(jti: str) -> None:
+#     await token_blocklist.set(name=jti, value="", ex=JTI_EXPIRY)
+
+
+# async def token_in_blocklist(jti:str) -> bool:
+#    jti =  await token_blocklist.get(jti)
+
+#    return jti is not None
+
+
+import redis.asyncio as redis
 from src.config import Config
 
 JTI_EXPIRY = 3600
 
-token_blocklist = aioredis.StrictRedis(
-    host=Config.REDIS_HOST, port=Config.REDIS_PORT, db=0
+token_blocklist = redis.Redis(
+    host=Config.REDIS_HOST,
+    port=Config.REDIS_PORT,
+    db=0
 )
 
 
 async def add_jti_to_blocklist(jti: str) -> None:
-    await token_blocklist.set(name=jti, value="", ex=JTI_EXPIRY)
+    await token_blocklist.set(
+        name=jti,
+        value="",
+        ex=JTI_EXPIRY
+    )
 
 
-async def token_in_blocklist(jti:str) -> bool:
-   jti =  await token_blocklist.get(jti)
+async def token_in_blocklist(jti: str) -> bool:
+    jti_entry = await token_blocklist.get(jti)
 
-   return jti is not None
+    return jti_entry is not None
