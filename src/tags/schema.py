dccf98faf5f86e0188f3ec78d34c03175017d483
thisIsOwais <mohdowaisansari608@gmail.com>
2026-05-22 19:13:30 +0530

    Add tags feature: implement tag model, routes, and service; integrate with book model for tag management

---
+++ src/tags/schema.py
@@ -0,0 +1,31 @@
+import uuid
+from datetime import date, datetime
+from typing import List, Optional
+import sqlalchemy.dialects.postgresql as pg
+from sqlmodel import Column, Field, Relationship, SQLModel
+from src.auth.schema import User
+from src.books.schema import Book
+... # the rest of the file
+
+class BookTag(SQLModel, table=True):
+    book_id: uuid.UUID = Field(default=None, foreign_key="books.uid", primary_key=True)
+    tag_id: uuid.UUID = Field(default=None, foreign_key="tags.uid", primary_key=True)
+
+
+class Tag(SQLModel, table=True):
+    __tablename__ = "tags"
+    uid: uuid.UUID = Field(
+        sa_column=Column(pg.UUID, nullable=False, primary_key=True, default=uuid.uuid4)
+    )
+    name: str = Field(sa_column=Column(pg.VARCHAR, nullable=False))
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    books: List["Book"] = Relationship(
+        link_model=BookTag,
+        back_populates="tags",
+        sa_relationship_kwargs={"lazy": "selectin"},
+    )
+
+    def __repr__(self) -> str:
+        return f"<Tag {self.name}>"
+
+... # the rest of the file
